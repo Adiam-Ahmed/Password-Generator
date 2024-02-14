b937b8c521d9eb6351e3a34d052164c68bf6e49d
Adiam Ahmed <adu.adu911@gmail.com>
2024-02-14 16:31:30 -0500

    modified files

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# Password-Generator
-generates password randomly according to preference by the length requested , what kind of character  
+#Password Generator
+This is a simple password generator tool written in JS. It allows users to generate strong random passwords of varying lengths according to their preferences.
+#Features
+Generates random passwords with customizable length
+Allow users to include or exclude specific characters, symbols, or types of characters in the generated password
+Provides an option to copy the generated password to the clipboard for easy usage
+#Getting Started
+Clone the repository:
+git clone https://github.com/your_username/password-generator.git
+Navigate to the project directory
+
+#Usage
+Once you run the project, you will be prompted to enter the desired password length. You will also have the option to include/exclude uppercase letters, lowercase letters, digits, and special characters. After providing your preferences, the generator will output a random password according to your specifications.
+
+Contributing
+Contributions are welcome! Please feel free to open a pull request or submit an issue if you find any bugs or have suggestions for improvements.
+License
+This project is licensed under the MIT License
--- script.js
+++ script.js
@@ -1,94 +1,92 @@
 // Array of special characters to be included in password
 var specialCharacters = [
-  '@',
-  '%',
-  '+',
-  '\\',
-  '/',
+  "@",
+  "%",
+  "+",
+  "\\",
+  "/",
   "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  '?',
-  ':',
-  ',',
-  ')',
-  '(',
-  '}',
-  '{',
-  ']',
-  '[',
-  '~',
-  '-',
-  '_',
-  '.'
+  "!",
+  "#",
+  "$",
+  "^",
+  "?",
+  ":",
+  ",",
+  ")",
+  "(",
+  "}",
+  "{",
+  "]",
+  "[",
+  "~",
+  "-",
+  "_",
+  ".",
 ];
 
 // Array of numeric characters to be included in password
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 // Array of lowercase characters to be included in password
 var lowerCasedCharacters = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z'
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
 ];
 
 // Array of uppercase characters to be included in password
 var upperCasedCharacters = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z'
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
 ];
-
-
 
 // Function to prompt user for password options
 //1st we let the user insert the length of the password (make sure to loop the user if they didnt insert length b/8 8 up to 128)
@@ -98,73 +96,75 @@ var upperCasedCharacters = [
 var passwordLength;
 var charset = "";
 
-
 function getPasswordOptions() {
-  passwordLength= parseInt(prompt("Please insert the password length,  Ensure it is atleast 8 character but no more than 128"));
-  while (passwordLength < 8 || passwordLength >128 ) {
-    passwordLength= parseInt(prompt("Ensure it is atleast 8 character but no more than 128, please re-insert the password length"));
+  passwordLength = parseInt(
+    prompt(
+      "Please insert the password length,  Ensure it is atleast 8 character but no more than 128",
+    ),
+  );
+  while (passwordLength < 8 || passwordLength > 128) {
+    passwordLength = parseInt(
+      prompt(
+        "Ensure it is atleast 8 character but no more than 128, please re-insert the password length",
+      ),
+    );
   }
   charset = "";
 
   let haslowerCase = confirm("would you like to include lowercase character");
 
-  if(haslowerCase){
+  if (haslowerCase) {
     charset += lowerCasedCharacters.join("");
   }
   let hasUpperCase = confirm("would you like to include uppercase character");
-  if(hasUpperCase){
+  if (hasUpperCase) {
     charset += upperCasedCharacters.join("");
   }
   let hasnumeric = confirm("would you like to include numeric character");
-  if(hasnumeric){
+  if (hasnumeric) {
     charset += numericCharacters.join("");
   }
   let hasSpecialCharac = confirm("would you like to include special character");
-  if(hasSpecialCharac){
+  if (hasSpecialCharac) {
     charset += specialCharacters.join("");
   }
   generatePassword(); // Generate password after options are set
   // console.log(charset)
 }
 
-
 // Function for getting a random element from the chareset
-//we pass in charset to the function as param then created a var that will select a random number which we can use as an index 
-//this function will return a random character from the charset 
+//we pass in charset to the function as param then created a var that will select a random number which we can use as an index
+//this function will return a random character from the charset
 
 function getRandom(charset) {
   // Generate a random index within the length of the array so to return the element at the random index
   var randomIndex = Math.floor(Math.random() * charset.length);
   return charset[randomIndex];
-
 }
 
 // Function to generate password with user input
 //1st we create an empty string to hold the generated password
-//2nd we enter a loop until we get the password length requested by the users 
+//2nd we enter a loop until we get the password length requested by the users
 //this loop will call the getrandom function and the character that will be selected will get concated into the empty string password
 // we created a var called passwordtext to target an HTML element with the ID "password" using the querySelector() method provided by DOM
 //then we target the passwordtext value to be the value of the variable password.
 
 function generatePassword() {
-  var password = '';
+  var password = "";
   for (var i = 0; i < passwordLength; i++) {
     password += getRandom(charset);
   }
-  var passwordText = document.querySelector('#password');
+  var passwordText = document.querySelector("#password");
   passwordText.value = password;
-
 }
 
 // Get references to the #generate element
-var generateBtn = document.querySelector('#generate');
+var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
   getPasswordOptions(); // Retrieve password options before generating
-
 }
 
-
 // Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
+generateBtn.addEventListener("click", writePassword);
